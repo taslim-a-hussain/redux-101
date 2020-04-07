77c398a9916209dede49690d24eba6674c25a5a2
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-07 15:04:49 +1200

    okay

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,2 +1,19 @@
 import {createStore, combineReducers} from 'redux';
 
+const state = {
+    expenses: [
+        {
+            id: "123abc8910",
+            description: 'Jan rent',
+            note: 'This was the final payment for the address',
+            amount: 31900,
+            createdAt: 0
+        }
+    ],
+    filter: {
+        text: 'rent',
+        sortBy: 'amount', // amount or date
+        startDate: undefined,
+        endDate: undefined
+    }
+};
