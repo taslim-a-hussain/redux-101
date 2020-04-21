e62981d09bb8b438edfed593b4e61c950bb9a439
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-21 13:16:28 +1200

    update

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './ExpenseForm.scss';
 
 export default class ExpenseForm extends React.Component {
 
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        error: '',
+        class: 'error'
     };
 
     onChangeDescription = (e) => {
@@ -26,7 +29,20 @@ export default class ExpenseForm extends React.Component {
         const amount = e.target.value;
         if (amount.match(/^\d*(\.*\d{0,2})?$/)) {
             this.setState(() => ({
-                amount
+                amount,
+                error: ''
+            }));
+        
+        } else if (amount.match(/^\d*(\.*\d{0,9})?$/)) {
+            const error = 'Number is already valid.';
+            this.setState(() => ({
+                error,
+                class: 'success'
+            }));
+        } else {
+            const error = 'Enter valid number eg. 249 or 249.00';
+            this.setState(() => ({
+                error
             }));
         }
     };
@@ -34,6 +50,7 @@ export default class ExpenseForm extends React.Component {
     render() {
         return (
             <div>
+                {this.state.error && <p className={this.state.class}>{this.state.error}</p>}
                 <form className="expense-form">
                     <input type="text" placeholder="Description" autoFocus value={this.state.description} onChange={this.onChangeDescription} />
                     <input type="text" placeholder="Amount" value={this.state.amount} onChange={this.onChangeAmount} />
