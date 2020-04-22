e0bd4c58eb804e1466261ba66919d3cf17513de9
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-22 21:39:35 +1200

    update

--- src/redux/selectors/expenses.js
+++ src/redux/selectors/expenses.js
@@ -1,7 +1,10 @@
+import moment from 'moment';
+
 export default (expenses, {text, sortBy, startDate, endDate}) => {
     return expenses.filter(expense => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const momentCreatedAt = moment(expense.createdAt);
+        const startDateMatch = startDate ? startDate.isSameOrBefore(momentCreatedAt, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(momentCreatedAt, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
