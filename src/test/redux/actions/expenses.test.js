3a6ec0c2b7e492a0798e56cfa9d2bde9c842faa1
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-22 23:09:07 +1200

    update

---
+++ src/test/redux/actions/expenses.test.js
@@ -0,0 +1,52 @@
+import {removeExpense, editExpense, addExpense} from '../../../redux/actions/expenses';
+
+test('Remove expense by an id', () => {
+    const result = removeExpense('1234');
+    expect(result).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '1234'
+    });
+});
+
+test('Edit expense by an id and passing a new expense obj', () => {
+    const result = editExpense('1234', {description: 'coffee'});
+    expect(result).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '1234',
+        update: {
+            description: 'coffee'
+        }
+    });
+});
+
+test('Add expense', () => {
+    const expense = {
+        description: 'coffee',
+        note: 'Morning coffee',
+        amount: 2.30,
+        createdAt: 3000
+    };
+    const result = addExpense(expense);
+    expect(result).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            ...expense
+        }
+    });
+});
+
+test('Add expense test defaults', () => {
+
+    const result = addExpense();
+    expect(result).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0
+        }
+    });
+});
